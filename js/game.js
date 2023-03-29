9ef9a68406161a1ec3eb3cf292291b0d6a2c71d5
Muhammed Akbas <md.akbas@gmx.de>
2023-03-29 17:02:04 +0200

    updated sprites

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@ function init(){
     canvas = document.getElementById('canvas')
     let ctx = canvas.getContext('2d');
 
-    character.src = "sprites/Samurai/Attack_1.png";
+    character.src = "hero/sprites/standing/tile000.png";
 
     setTimeout(function(){
         ctx.drawImage(character, 20, 20, 50, 150);
