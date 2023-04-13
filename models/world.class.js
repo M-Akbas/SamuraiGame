8f95e877944869a2464aba62ec1c4e7452e38232
Muhammed Akbas <md.akbas@gmx.de>
2023-04-13 19:43:54 +0200

    dwsign & structure lvls

--- models/world.class.js
+++ models/world.class.js
@@ -1,21 +1,20 @@
 class World {
   // Everthing what in world contains
   character = new Character();
-  
+
   enemies = level1.enemies;
   background = level1.background;
   floor = level1.floor;
-  secondFloor = level1.secondfloor;
+  secondFloor = level1.secondFloor;
   fence = level1.fence;
   lamps = level1.lamps;
   rocks = level1.rocks;
   shop = level1.shop;
-  
+
   canvas;
   ctx;
   keyboard;
   camera_x = 0;
-  
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -30,7 +29,7 @@ class World {
   }
 
   draw() {
-    this.ctx.clearRect(0, 0,  this.canvas.height, this.canvas.width,);
+    this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
 
     this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.background);
@@ -40,11 +39,10 @@ class World {
     this.addObjectsToMap(this.lamps);
     this.addObjectsToMap(this.rocks);
     this.addObjectsToMap(this.shop);
-
+    
     this.addObjectsToMap(this.enemies);
 
     this.addToMap(this.character);
-
     this.ctx.translate(-this.camera_x, 0);
 
     // Draw() wird immer wieder getriggerd
