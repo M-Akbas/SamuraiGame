7c42674d6b8a15837edf6a7bff4a6467f6a2581e
Muhammed Akbas <md.akbas@gmx.de>
2023-05-17 18:06:27 +0200

    clean coded

--- models/world.class.js
+++ models/world.class.js
@@ -1,13 +1,13 @@
 class World {
-  // Everthing what in world contains
 
   swordSounds = [
     new Audio("audio/hits/hit1.mp3"),
     new Audio("audio/hits/hit2.mp3"),
     new Audio("audio/hits/hit3.mp3"),
   ];
+
   enmieHurtSound = new Audio("audio/hurt/enemieHUrt.mp3");
-  throwSound = new Audio("audio/jump/throw.sound.mp3")
+  throwSound = new Audio("audio/jump/throw.sound.mp3");
   ShurikenCounter = [];
   throwableObject = [];
   character = new Character();
@@ -20,6 +20,7 @@ class World {
   statusbar = new StatusBar();
   heartIcon = new HeartIcon("healthbar/health.png", 10, 20);
   shurikenIcon = new Shuriken("imgForDesign/shurikenpixel.png", 10, 100);
+
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
@@ -31,58 +32,60 @@ class World {
     this.checkLocation();
     this.checkThrowObject();
     this.drawNewShuriken();
+    this.endOfGame();
   }
+
+
+  endOfGame() {
+    let endboss = this.level.endboss[0];
+    setInterval(() => {
+      this.level.enemies.forEach((enemy) => {
+        if (
+          (endboss.energy == 0 && enemy.energy == 0) ||
+          this.character.energy == 0
+        ) {
+          console.log("Game ende");
+        }
+      });
+    });
+  }
+
 
   drawNewShuriken() {
     setInterval(() => {
       if (this.ShurikenCounter.length > 0) {
-        console.log("true");
         this.addObjectsToMap(this.ShurikenCounter);
-      } else {
-        console.log("false");
-      }
+      } 
     });
   }
+
+
   checkThrowObject() {
     let i = 0;
     let endboss = this.level.endboss[0];
     let newShuriken;
-  
+
     setInterval(() => {
       if (this.keyboard.D) {
-        if (this.ShurikenCounter.length > 0 && i < 4) { 
-          let shuriken = new ThrowableObject(this.character.x + 190, this.character.y + 100);
+        if (this.ShurikenCounter.length > 0 && i < 4) {
+          let shuriken = new ThrowableObject(
+            this.character.x + 190,
+            this.character.y + 100
+          );
           this.throwableObject.push(shuriken);
-  
           newShuriken = this.throwableObject[i];
           newShuriken.throw();
           this.throwSound.play();
-          
-
           this.ShurikenCounter.shift();
           i++;
-
-         
         }
       }
-  
       this.level.enemies.forEach((enemy) => {
-        if (newShuriken && (newShuriken.isColliding(endboss) || newShuriken.isColliding(enemy))) {
-          enemy.hurtAnimation();
-          endboss.hurtAnimation();
-          endboss.hitDamage(1);
-          enemy.hitDamage(2);
-          this.enmieHurtSound.play();
-          console.log(endboss.energy);
-          console.log(enemy.energy);
-        }
+        this.handleCollisions(newShuriken, endboss, enemy);
       });
-
-      
     }, 100);
   }
-  
-  
+
 
   checkCollisions() {
     let endboss = this.level.endboss[0];
@@ -90,127 +93,60 @@ class World {
     setInterval(() => {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isColliding(enemy)) {
-          if (enemy.energy == 0) {
-            // stay there
-          } else if (enemy.isFighting == true) {
-            this.character.hit();
-            this.statusbar.setPercentage(this.character.energy);
-            this.swordSounds.forEach((sound) => sound.play());
-            enemy.animationForEnemie();
-          }
+          this.isFighting(enemy);
         }
         if (this.character.isColliding(endboss)) {
-          if (endboss.energy == 0) {
-            let currentX = endboss.x;
-            endboss.x = currentX;
-          } else {
-            this.character.hit();
-            this.statusbar.setPercentage(this.character.energy);
-            this.swordSounds.forEach((sound) => sound.play());
-          }
+          this.deadOrAlive(endboss);
         }
         if (
           this.character.world.keyboard.space &&
           this.character.isColliding(enemy)
         ) {
-          if (enemy.energy == 0) {
-          } else {
-            enemy.isFighting = false;
-            enemy.hurtAnimation();
-            this.level.enemies.forEach((enemy) => {
-              if (this.character.isColliding(enemy)) {
-                enemy.hitDamage(1);
-              }
-            });
-          }
+          this.fightEnemie(enemy);
         }
         if (
           this.character.world.keyboard.space &&
           this.character.isColliding(endboss)
         ) {
-          if (endboss.energy == 0) {
-            setTimeout(() => {}, 100);
-          } else {
-            endboss.isFighting = false;
-            endboss.hurtAnimation();
-            endboss.hitDamage(1);
-          }
+          this.fightEndboss(endboss);
         }
         if (!this.character.world.keyboard.space) {
-          endboss.isFighting = true;
-          enemy.isFighting = true;
+          this.enemiesAreFighting(enemy, endboss);
         }
 
         if (this.character.isColliding(endboss)) {
-          if (endboss.energy == 0) {
-            // do nothing
-          } else if (endboss.isFighting == true) {
-            endboss.attackAnimation();
-            this.swordSounds.forEach((sound) => sound.play());
-          }
+          this.endbossAttacking(endboss);
         }
       });
 
-      for (let i = 0; i < this.level.shuriken.length; i++) {
-        let shuriken = this.level.shuriken[i];
-        if (this.character.isColliding(shuriken)) {
-          this.level.shuriken.splice(i, 1);
-          this.ShurikenCounter.push(shuriken);
-
-          break;
-        }
-      }
-      
+      this.isShurikenCollected();
     }, 100);
   }
+ 
 
   checkLocation() {
     const endboss = this.level.endboss[0];
     setInterval(() => {
       this.level.enemies.forEach((enemy) => {
-        if (enemy.energy == 0) {
-          // do nothing
-        } else if (this.character.x !== enemy.x) {
-          const dx = this.character.x - enemy.x;
-          const distance = Math.abs(dx);
-          const speed = enemy.speed;
-          if (dx > 0) {
-            enemy.x += (dx * speed * 10) / distance + 1;
-            enemy.otherDirection = true;
-          } else {
-            enemy.otherDirection = false;
-          }
-        }
+        this.enemyFollowChar(enemy);
       });
-
-      // Check end boss location
-      if (endboss.energy == 0) {
-        // do nothing
-      } else if (this.character.x !== endboss.x) {
-        const dx = this.character.x - endboss.x;
-        const distance = Math.abs(dx);
-        const speed = endboss.speed;
-        if (dx > 0) {
-          endboss.x += (dx * speed * 10) / distance + 1;
-          endboss.otherDirection = true;
-        } else {
-          endboss.otherDirection = false;
-        }
-      }
+      this.endbossFollowChar(endboss);
     }, 50);
   }
+ 
 
   setWorld() {
     this.character.world = this;
   }
 
+
   music() {
     this.backgroundMusic.play();
   }
 
+
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
-
     this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.level.background);
     this.addObjectsToMap(this.level.fence);
@@ -221,28 +157,25 @@ class World {
     this.addToMap(this.heartIcon);
     this.addToMap(this.shurikenIcon);
     this.ctx.font = "30px 'Press Start 2P'";
-    this.ctx.fillStyle = "rgba(255, 255, 255, 0.7)"; // Farbe des Textes auf ein etwas transparentes Weiß-Grau setzen
+    this.ctx.fillStyle = "rgba(255, 255, 255, 0.7)"; 
     this.ctx.fillText(this.ShurikenCounter.length, 100, 140);
-
     this.ctx.translate(this.camera_x, 0);
-
     this.addObjectsToMap(this.level.secondFloor);
     this.addObjectsToMap(this.level.lamps);
     this.addObjectsToMap(this.level.rocks);
     this.addObjectsToMap(this.level.shop);
-
     this.addToMap(this.character);
     this.addObjectsToMap(this.level.enemies);
     this.addObjectsToMap(this.level.shuriken);
     this.addObjectsToMap(this.level.endboss);
     this.ctx.translate(-this.camera_x, 0);
 
-    // Draw() wird immer wieder getriggerd
     self = this;
     requestAnimationFrame(function () {
       self.draw();
     });
   }
+
 
   addToMap(mo) {
     if (mo.otherDirection) {
@@ -257,11 +190,13 @@ class World {
     }
   }
 
+
   addObjectsToMap(objects) {
     objects.forEach((o) => {
       this.addToMap(o);
     });
   }
+
 
   flipImage(mo) {
     this.ctx.save();
@@ -270,8 +205,136 @@ class World {
     mo.x = mo.x * -1;
   }
 
+
   flipImageBack(mo) {
     this.ctx.restore();
     mo.x = mo.x * -1;
+  }
+
+
+  handleCollisions(newShuriken, endboss, enemy) {
+    if (
+      newShuriken &&
+      (newShuriken.isColliding(endboss) || newShuriken.isColliding(enemy))
+    ) {
+      enemy.hurtAnimation();
+      endboss.hurtAnimation();
+      endboss.hitDamage(1);
+      enemy.hitDamage(2);
+      this.enmieHurtSound.play();
+      console.log(endboss.energy);
+      console.log(enemy.energy);
+    }
+  }
+
+
+  isFighting(enemy) {
+    if (enemy.energy == 0) {
+      // stay there
+    } else if (enemy.isFighting == true) {
+      this.character.hit(enemy);
+      this.statusbar.setPercentage(this.character.energy);
+      this.swordSounds.forEach((sound) => sound.play());
+      enemy.animationForEnemie();
+    }
+  }
+
+
+  fightEnemie(enemy) {
+    if (enemy.energy == 0) {
+    } else {
+      enemy.isFighting = false;
+      enemy.hurtAnimation();
+      this.level.enemies.forEach((enemy) => {
+        if (this.character.isColliding(enemy)) {
+          enemy.hitDamage(1);
+        }
+      });
+    }
+  }
+
+
+  fightEndboss(endboss) {
+    if (endboss.energy == 0) {
+      setTimeout(() => {}, 100);
+    } else {
+      endboss.isFighting = false;
+      endboss.hurtAnimation();
+      endboss.hitDamage(1);
+    }
+  }
+
+
+  enemiesAreFighting(enemy, endboss) {
+    endboss.isFighting = true;
+    enemy.isFighting = true;
+  }
+
+
+  endbossAttacking(endboss){
+    if (endboss.energy == 0) {
+      // do nothing
+    } else if (endboss.isFighting == true) {
+      endboss.attackAnimation();
+      this.swordSounds.forEach((sound) => sound.play());
+    }
+  }
+
+
+  isShurikenCollected(){
+    for (let i = 0; i < this.level.shuriken.length; i++) {
+      let shuriken = this.level.shuriken[i];
+      if (this.character.isColliding(shuriken)) {
+        this.level.shuriken.splice(i, 1);
+        this.ShurikenCounter.push(shuriken);
+
+        break;
+      }
+    }
+  }
+
+
+  deadOrAlive(endboss) {
+    if (endboss.energy == 0) {
+      let currentX = endboss.x;
+      endboss.x = currentX;
+    } else {
+      this.character.hit();
+      this.statusbar.setPercentage(this.character.energy);
+      this.swordSounds.forEach((sound) => sound.play());
+    }
+  }
+
+  enemyFollowChar(enemy){
+    if (enemy.energy == 0) {
+      // do nothing
+    } else if (this.character.x !== enemy.x) {
+      const dx = this.character.x - enemy.x;
+      const distance = Math.abs(dx);
+      const speed = enemy.speed;
+      if (dx > 0) {
+        enemy.x += (dx * speed * 10) / distance + 1;
+        enemy.otherDirection = true;
+      } else {
+        enemy.otherDirection = false;
+      }
+    }
+  }
+
+
+  endbossFollowChar(endboss){
+    if (endboss.energy == 0) {
+      // do nothing
+    } else if (this.character.x !== endboss.x) {
+      const dx = this.character.x - endboss.x;
+      const distance = Math.abs(dx);
+      const speed = endboss.speed;
+      if (dx > 0) {
+        endboss.x += (dx * speed * 10) / distance + 1;
+        endboss.otherDirection = true;
+      } else {
+        endboss.otherDirection = false;
+      }
+    }
   }
 }
